27eabfc3c2db30ff0bb672e80922f72f8598affb
Oskar Kurczewski <229935@edu.p.lodz.pl>
2024-01-26 00:50:08 +0100

    change data path to correct one again

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import RatingComparisonChart from '../components/RatingComparisonChart';
 import * as d3 from 'd3';
 
 const fetchData = async () => {
-  const rawData = await d3.dsv(";", "/api/albums.csv");
+  const rawData = await d3.dsv(";", "https://vercel.com/api/v7/deployments/dpl_9Ytz5TWBrha6Jszg9yAY2Zq6t79r/files/get?path=public%2Falbums.csv");
 
   const albums = rawData.map(({ album, current_price, release_date, rym_user_rating, rym_own_rating }) => ({
     album,
